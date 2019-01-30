60d278859a5de8fd4a1fd30012c21f360e6f2ab7
Bubaluza <gabrielschiller8@gmail.com>
2019-01-30 00:08:13 -0200

    'fundamentos' progress

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,27 @@
+// novo recurso es2015
+
+const pessoa ={
+    nome: 'Ana',
+    idade: 5,
+    endereço: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade} = pessoa
+console.log( nome, idade)
+
+const {nome: n, idade: i} = pessoa 
+console.log(n,i)
+
+const{ sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const {endereço: {logradouro , numero, cep} } = pessoa 
+
+console.log(logradouro, numero, cep)
+
+const { conta : { ag, num}} = pessoa            // vai da errado pois conta não existe
+console.log(ag, num)
+
---
+++ fundamentos/hoisting.js
@@ -0,0 +1,7 @@
+console.log('a =', a)
+var a = 2
+console.log('a = ',a)
+
+//console.log(' b =', b)          //vai gerar erro pois b não terá um hoisting para cima
+//let b = 2
+//console.log(' b =', b)        
